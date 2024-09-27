70996a33b1d734797da85c6cc9fe44831affec20
Kyle Nazario <knazario@apple.com>
2024-09-27 16:37:01 -0600

    More declarative joke format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Signal, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DadJokeService } from './dad-joke.service';
 import { JsonPipe } from '@angular/common';
 import { JokeSearchComponent } from './joke-search/joke-search.component';
+import { map, Subject, switchMap } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
@@ -14,13 +16,14 @@ import { JokeSearchComponent } from './joke-search/joke-search.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  protected joke = signal<string | undefined>(undefined);
+  protected joke: Signal<string>;
+  protected readonly loadNewJokeButtonClicked$ = new Subject<void>();
 
-  constructor(readonly dadJokeService: DadJokeService) {}
-
-  protected loadNewJoke() {
-    this.dadJokeService.getRandomJoke().subscribe((response) => {
-      this.joke.set(response.joke);
-    });
+  constructor(readonly dadJokeService: DadJokeService) {
+    const joke$ = this.loadNewJokeButtonClicked$.pipe(
+      switchMap(() => dadJokeService.getRandomJoke()),
+      map((response) => response.joke)
+    )
+    this.joke = toSignal(joke$, {initialValue: ''});
   }
 }
